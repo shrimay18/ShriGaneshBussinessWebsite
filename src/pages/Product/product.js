224405aa702a5604d851659fe0420ad06dbe0c5e
shrimay18 <shrimaysomani18@gmail.com>
2024-10-24 12:21:01 +0530

    cart & product page made responsive

--- src/pages/Product/product.js
+++ src/pages/Product/product.js
@@ -4,6 +4,8 @@ import Navbar from '../../components/Navbar/navbar';
 import { useState, useEffect } from 'react';
 import ProductCard from '../../components/ProductCard/productCard';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCartShopping} from '@fortawesome/free-solid-svg-icons';
 
 
 function Product() {
@@ -33,7 +35,9 @@ function Product() {
             <div className='product-container'>
                 <div className='product-search'>
                     <input type='text' placeholder='Search for products' value={search} onChange={(e)=>{setSearch(e.target.value)}}/>
-                    <button className='add-to-cart-button' onClick={getToCart} >Your Cart</button>
+                    <button className='add-to-cart-button' onClick={getToCart} >Your Cart 
+                        <FontAwesomeIcon icon={faCartShopping} />
+                    </button>
                 </div>
                 <div className='product-list'>
                     {filteredProducts.map((product) => {
